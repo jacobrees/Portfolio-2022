b6bb58831b8a35acc45f7c9e54670242d5fbe8aa
jacobrees <jacob.rees97@yahoo.com>
2022-11-19 14:30:51 +0000

    Uninstall gsap due to page sizing bug on mobile devices

--- app.js
+++ app.js
@@ -1,14 +1,3 @@
-/*eslint-disable */
-gsap.from('.project-fade-right', {
-  scrollTrigger: { trigger: '.project-fade-right' }, duration: 1, x: 100, opacity: 0, stagger: 0.2, delay: 0.5,
-});
-gsap.from('.project-fade-left', {
-  scrollTrigger: { trigger: '.project-fade-left' }, duration: 1, x: -100, opacity: 0, stagger: 0.2, delay: 0.5,
-});
-gsap.from('.project-fade-right2', {
-  scrollTrigger: { trigger: '.project-fade-right2' }, duration: 1, x: 100, opacity: 0, stagger: 0.2, delay: 0.5,
-});
-/* eslint-enable */
 window.scrollTo(0, 0);
 const menu = document.querySelector('.hamburger-menu');
 const mobileNav = document.querySelector('.mobile-navbar');
