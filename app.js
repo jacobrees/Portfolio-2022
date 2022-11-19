25300162422884dad8c50854a2c77f117f0aad5d
jacobrees <jacob.rees97@yahoo.com>
2022-11-19 03:07:40 +0000

    Change animations to affect about me text instead of section to fix screen sizing bug on mobile

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 /* eslint-disable */
 gsap.from('.home-text', {duration: 1, y: -50, opacity: 0, delay: 0.5, ease: 'power4.out', stagger: 0.3});
 gsap.from('.my-photo', {duration: 1, y: 50, opacity: 0, delay: 0.5, ease: 'power4.out', stagger: 0.3});
-gsap.from('.about-me-section', {duration: 1, y: 50, opacity: 0, delay: 0.5, ease: 'power4.out', stagger: 0.3});
+gsap.from('.about-me-title', {duration: 1, y: 50, opacity: 0, delay: 0.5, ease: 'power4.out', stagger: 0.3});
+gsap.from('.about-me-text', {duration: 1, y: 50, opacity: 0, delay: 0.5, ease: 'power4.out', stagger: 0.3});
 gsap.from('.showcase-title', {scrollTrigger: '.showcase-title', duration: 1, x: -50, opacity: 0, delay: 0.5, ease: 'power4.out', stagger: 0.3});;
 gsap.from('.project-fade-right', {scrollTrigger: '.project-fade-right', duration: 1, x: -50, opacity: 0, delay: 0.5, ease: 'power4.out', stagger: 0.3});
 gsap.from('.project-fade-left', {scrollTrigger: '.project-fade-left', duration: 1, x: 50, opacity: 0, delay: 0.5, ease: 'power4.out', stagger: 0.3});
