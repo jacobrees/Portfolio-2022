d70cb1bbcd280ce3198095e530f96086f7caf4f3
jacobrees <jacob.rees97@yahoo.com>
2023-03-27 21:39:05 +0100

    Fix javascript linting errors

--- app.js
+++ app.js
@@ -33,6 +33,6 @@ navLinks.forEach((navLink) => {
 });
 
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'assets/particlesjs-config.json', function() {
-  console.log('callback - particles.js config loaded');
+particlesJS.load("particles-js", "assets/particlesjs-config.json", function () {
+  console.log("callback - particles.js config loaded");
 });
