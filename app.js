677c5aef9a3bb7b3a749578a884e63d1b0bb10d0
jacobrees <jacob.rees97@yahoo.com>
2023-03-27 21:36:35 +0100

    Updated portfolio to include partles.js

--- app.js
+++ app.js
@@ -1,33 +1,38 @@
 window.scrollTo(0, 0);
-const menu = document.querySelector('.hamburger-menu');
-const mobileNav = document.querySelector('.mobile-navbar');
-const mobileNavLinks = document.querySelectorAll('.mobile-nav-links');
+const menu = document.querySelector(".hamburger-menu");
+const mobileNav = document.querySelector(".mobile-navbar");
+const mobileNavLinks = document.querySelectorAll(".mobile-nav-links");
 
-menu.addEventListener('click', () => {
-  if (mobileNav.style.visibility === 'visible') {
-    mobileNav.style.visibility = 'hidden';
+menu.addEventListener("click", () => {
+  if (mobileNav.style.visibility === "visible") {
+    mobileNav.style.visibility = "hidden";
   } else {
-    mobileNav.style.visibility = 'visible';
+    mobileNav.style.visibility = "visible";
   }
 });
 
 mobileNavLinks.forEach((navLink) => {
-  navLink.addEventListener('click', () => {
-    mobileNav.style.visibility = 'hidden';
+  navLink.addEventListener("click", () => {
+    mobileNav.style.visibility = "hidden";
   });
 });
 
-const navLinks = document.querySelectorAll('.nav-link');
+const navLinks = document.querySelectorAll(".nav-link");
 
 navLinks.forEach((navLink) => {
-  navLink.addEventListener('click', (e) => {
+  navLink.addEventListener("click", (e) => {
     e.preventDefault();
     const id = navLink.textContent;
     const section = document.getElementById(id);
     const position = section.offsetTop - 70;
     window.scrollTo({
       top: position,
-      behavior: 'smooth',
+      behavior: "smooth",
     });
   });
+});
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'assets/particlesjs-config.json', function() {
+  console.log('callback - particles.js config loaded');
 });
