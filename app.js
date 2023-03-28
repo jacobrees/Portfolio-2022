6be597f8212d725a9dfb9762660da9af63715beb
jacobrees <jacob.rees97@yahoo.com>
2023-03-28 14:55:12 +0100

    Add animation to navbar to scroll in from the side, add animated hamburger to cross icon

--- app.js
+++ app.js
@@ -1,37 +1,34 @@
 window.scrollTo(0, 0);
-const menu = document.querySelector('.hamburger-menu');
-const mobileNav = document.querySelector('.mobile-navbar');
-const mobileNavLinks = document.querySelectorAll('.mobile-nav-links');
+const menu = document.querySelector(".hamburger-menu");
+const mobileNav = document.querySelector(".mobile-navbar");
+const mobileNavLinks = document.querySelectorAll(".mobile-nav-links");
 
-menu.addEventListener('click', () => {
-  if (mobileNav.style.visibility === 'visible') {
-    mobileNav.style.visibility = 'hidden';
-  } else {
-    mobileNav.style.visibility = 'visible';
-  }
+menu.addEventListener("click", () => {
+  menu.classList.toggle('open');
+  mobileNav.classList.toggle("show");
 });
 
 mobileNavLinks.forEach((navLink) => {
-  navLink.addEventListener('click', () => {
-    mobileNav.style.visibility = 'hidden';
+  navLink.addEventListener("click", () => {
+    mobileNav.classList.toggle("show");
   });
 });
 
-const navLinks = document.querySelectorAll('.nav-link');
+const navLinks = document.querySelectorAll(".nav-link");
 
 navLinks.forEach((navLink) => {
-  navLink.addEventListener('click', (e) => {
+  navLink.addEventListener("click", (e) => {
     e.preventDefault();
     const id = navLink.textContent;
     const section = document.getElementById(id);
     const position = section.offsetTop - 70;
     window.scrollTo({
       top: position,
-      behavior: 'smooth',
+      behavior: "smooth",
     });
   });
 });
 
-particlesJS.load('particles-js', 'assets/particlesjs-config.json', () => { // eslint-disable-line
-
+particlesJS.load("particles-js", "assets/particlesjs-config.json", () => {
+  // eslint-disable-line
 });
