9717d07a500f026a01309833e9972191fb392ae7
jacobrees <jacob.rees97@yahoo.com>
2023-03-28 15:03:12 +0100

    Fix eslint errors

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
 window.scrollTo(0, 0);
-const menu = document.querySelector(".hamburger-menu");
-const mobileNav = document.querySelector(".mobile-navbar");
-const mobileNavLinks = document.querySelectorAll(".mobile-nav-links");
+const menu = document.querySelector('.hamburger-menu');
+const mobileNav = document.querySelector('.mobile-navbar');
+const mobileNavLinks = document.querySelectorAll('.mobile-nav-links');
 
-menu.addEventListener("click", () => {
-  menu.classList.toggle("open");
-  mobileNav.classList.toggle("show");
+menu.addEventListener('click', () => {
+  menu.classList.toggle('open');
+  mobileNav.classList.toggle('show');
 });
 
 mobileNavLinks.forEach((navLink) => {
-  navLink.addEventListener("click", () => {
-    menu.classList.toggle("open");
-    mobileNav.classList.toggle("show");
+  navLink.addEventListener('click', () => {
+    menu.classList.toggle('open');
+    mobileNav.classList.toggle('show');
   });
 });
 
-const navLinks = document.querySelectorAll(".nav-link");
+const navLinks = document.querySelectorAll('.nav-link');
 
 navLinks.forEach((navLink) => {
-  navLink.addEventListener("click", (e) => {
+  navLink.addEventListener('click', (e) => {
     e.preventDefault();
     const id = navLink.textContent;
     const section = document.getElementById(id);
     const position = section.offsetTop - 70;
     window.scrollTo({
       top: position,
-      behavior: "smooth",
+      behavior: 'smooth',
     });
   });
 });
 
-particlesJS.load("particles-js", "assets/particlesjs-config.json", () => {
-  // eslint-disable-line
+particlesJS.load('particles-js', 'assets/particlesjs-config.json', () => {// eslint-disable-line
+  
 });
